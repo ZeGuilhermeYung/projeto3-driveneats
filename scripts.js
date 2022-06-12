42b98b9c7260da14617c9407ed2f63093eeddcdd
ZeGuilhermeYung <jgryung@gmail.com>
2022-06-12 02:11:35 -0300

    variação fechar pedido

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-function selecionarPedido(pedido, menu) {
+function selecionarPedido (pedido, menu) {
     let pedido1Clicado = document.querySelector(".pedido1.selecionado");
     let pedido2Clicado = document.querySelector(".pedido2.selecionado");
     let pedido3Clicado = document.querySelector(".pedido3.selecionado");
+    let contador = 0;
     if (pedido1Clicado !== null && menu == 1) {
         pedido1Clicado.classList.remove("selecionado");
     } else if (pedido2Clicado !== null && menu == 2) {
@@ -10,10 +11,25 @@ function selecionarPedido(pedido, menu) {
         pedido3Clicado.classList.remove("selecionado");  
     } 
     pedido.classList.add("selecionado");
+
     pedido1Clicado = document.querySelector(".pedido1.selecionado");
     pedido2Clicado = document.querySelector(".pedido2.selecionado");
     pedido3Clicado = document.querySelector(".pedido3.selecionado");
-    if (pedido1Clicado !== null && pedido2Clicado !== null && pedido3Clicado !== null) {
+    if (pedido1Clicado !== null) {
+        contador ++;
+    }
+    if (pedido2Clicado !== null) {
+        contador ++;
+    }
+    if (pedido3Clicado !== null) {
+        contador ++;
+    }
+    const pedidoAberto = document.querySelector(".rodape > div > h3");
+    if (contador == 1) {
+        pedidoAberto.innerHTML = "Selecione, ainda, 2 itens<br>para fechar o pedido";
+    } else if (contador == 2) {
+        pedidoAberto.innerHTML = "Selecione, ainda, um item<br>para fechar o pedido";
+    } else if (pedido1Clicado !== null && pedido2Clicado !== null && pedido3Clicado !== null) {
         const fecharPedido = document.querySelector(".rodape > div");
         fecharPedido.classList.add("fechado");
         const textoFecharPedido = document.querySelector(".rodape > div > h3");
