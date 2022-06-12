f639030419715a9ac88d30798cc12f81f6d3f421
ZeGuilhermeYung <jgryung@gmail.com>
2022-06-11 21:02:36 -0300

    pedidos clicáveis

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
-function selecionarPedido1(pedido1){
-    const pedido1Clicado = document.querySelector(".selecionado");
-    if (pedido1Clicado !== null) {
+function selecionarPedido(pedido, menu) {
+    const pedido1Clicado = document.querySelector(".pedido1.selecionado");
+    const pedido2Clicado = document.querySelector(".pedido2.selecionado");
+    const pedido3Clicado = document.querySelector(".pedido3.selecionado");
+    if (pedido1Clicado !== null && menu == 1) {
         pedido1Clicado.classList.remove("selecionado");
+    } else if (pedido2Clicado !== null && menu == 2) {
+        pedido2Clicado.classList.remove("selecionado");  
+    } else if (pedido3Clicado !== null && menu == 3) {
+        pedido3Clicado.classList.remove("selecionado");  
     }
-    pedido1.classList.add("selecionado");
+    pedido.classList.add("selecionado");
 }
